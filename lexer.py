d591d893814ab8ae46ab93866a2e9d0221896415
Francisco Veiga <francisco2.veiga@gmail.com>
2023-06-10 20:11:24 -0300

    funciona?

--- lexer.py
+++ lexer.py
@@ -23,32 +23,77 @@ tokens = {
 
 
 def lexer(programa):
+    programa = programa.strip()
     tokens_out = []
     tokens_posibles = [t for t in tokens]
     tokens_posibles_1mas = tokens_posibles.copy()
     lexema = ""
     lexema1mas = ""
-    for i in range(len(programa) - 1):
-        lexema += programa[i]
-        lexema1mas = lexema + programa[i+1]
+    for i in range(len(programa)):
+        tokens_final = []
+
+        # lexema += programa[i]
+        lexema = lexema1mas
+        lexema1mas = lexema1mas + programa[i]
         tokens_posibles = tokens_posibles_1mas.copy()
+
+        if lexema == " " or lexema == "\n":
+            # print("salteado espacio\n")
+            lexema1mas = programa[i]
+            continue
+
         for token in tokens_posibles:
             estado_1mas = tokens[token](lexema1mas)
             if estado_1mas == "TRAMPA":
                 tokens_posibles_1mas.remove(token)
-        print("lexema: ", lexema)
-        print("lexema 1 mas: ", lexema1mas)
-        print("Tokens posibles: ", tokens_posibles)
-        print("Tokens 1 mas   : ", str(tokens_posibles_1mas) + "\n")
+            estado_actual = tokens[token](lexema)
+            if estado_actual == "FINAL":
+                tokens_final.append(token)
+
+        # print(f"lexema: \"{lexema}\"")
+        # print(f"lexema 1 mas: lexema: \"{lexema1mas}\"")
+        # print("Tokens posibles: ", tokens_posibles)
+        # print("Tokens 1 mas   : ", str(tokens_posibles_1mas))
+        # print("Tokens finales : ", str(tokens_final) + "\n")
+
         if tokens_posibles_1mas == []:
-            tokens_out.append(tokens_posibles[0])
-            tokens_posibles = [t for t in tokens]
-            tokens_posibles_1mas = tokens_posibles.copy()
-            lexema = ""
+            if tokens_final != []:
+                tokens_out.append(tokens_final[0])
+                tokens_posibles = [t for t in tokens]
+                tokens_posibles_1mas = tokens_posibles.copy()
+                lexema1mas = programa[i]
+            else:
+                raise ValueError()
+
+    error = True
+    for token in tokens_posibles_1mas:
+        estado_actual = tokens[token](lexema1mas)
+        # print(lexema1mas)
+        # print(estado_actual)
+        if estado_actual == "FINAL":
+            tokens_out.append(token)
+            error = False
+            break
+        else:
+            continue
+    if error:
+        raise ValueError()
 
     return tokens_out
 
 
-programa = "hola = 45"
+programa = '''
+x=98234;
 
+
+si 6 < 7 entonces
+    x = x / 4;
+sino
+    x = x / 3;
+finsi
+    
+
+'''
+
+print(f"input: {programa}")
 print(lexer(programa))
